2f82853f7c328ffa44428662a57ed2eadb5fef59
rampsad27 <sainamaskey27@gmail.com>
2024-08-09 22:13:33 +0545

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,182 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+document.addEventListener('DOMContentLoaded', () => { //yo
+  const tabButtons = document.querySelectorAll('.tab-button');
+  const tabContents = document.querySelectorAll('.tab-content');
+
+  tabButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const target = button.dataset.tab;
+
+      tabButtons.forEach(btn => btn.classList.remove('active'));
+      tabContents.forEach(content => content.classList.remove('active'));
+
+      button.classList.add('active');
+      document.querySelector(`.tab-content.${target}`).classList.add('active');
+    });
+  });
+});
+
+
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== SHADOW HEADER ===============*/
+const shadowHeader = () =>{
+    const header = document.getElementById('header')
+    // When the shadow is greater than 50 viewport height, add the shadow-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('shadow-header') 
+                       : header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll', shadowHeader)
+
+/*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById("contact-form"),
+contactMessage = document.getElementById("contact-message");
+
+const sendEmail = (e) => {
+    e.preventDefault();
+
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_qi2dpxg', 'template_s6lhuoh', '#contact-form', 'wyDgy5YFd8Z9ZnDys').then(() => {
+        // show sent message
+        contactMessage.textContent = "Message sent successfully ✅"
+
+        // remove msg after 6s
+        setTimeout(() => {
+            contactMessage.textContent = '';
+        }, 6000);
+
+        // clear input fields
+        contactForm.reset();
+
+        // 
+    }, () => {
+        // show error if fails
+        contactMessage.textContent = "Message not sent (service error) ❌";
+    })
+}
+
+contactForm.addEventListener('submit', sendEmail)
+/*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*=============== SCROLL SECTIONS ACTIVE ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
+/*=============== DARK MODE ===============*/
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+
+// accept and decline to read../
+const acceptBtn = document.querySelector(".about__card__mobile-accept");
+const declineBtn = document.querySelector(".about__card__mobile-decline");
+const showTech = document.querySelector(".tech-list");
+const showCard = document.querySelector(".about__card");
+const card = document.querySelector(".about__card__mobile");
+const pref = document.querySelector(".about__card__mobile-prefs");
+
+pref.addEventListener("click", () => {
+    alert("Click on your desired button in the card to know more about my work")
+})
+showstck = () => {
+    showTech.style.display = "block";
+    showCard.style.display = "block"
+    card.style.display = "none";
+   
+}
+
+hidestck = () => {
+    alert("Ok i guess");
+}
+
+/*=============== SCROLL REVEAL ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 1500,
+    delay: 10,
+    reset: true //animation repeat
+})
+
+sr.reveal(`.home__perfil, .contact__mail`, { origin: 'right' })
+sr.reveal(`.home__name, .home__info, .home_about, .about__container, .section__title-1, .contact__data`, { origin: 'left' })
+sr.reveal( `.about__card__mobile`, { origin: 'bottom' })
+sr.reveal(`.home__perfil`, { origin: 'right' })
+sr.reveal(`.services__card, .projects__card, .clients__card`, { interval: 90 })
